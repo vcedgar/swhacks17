a47882bb2285abd8e85c62132619c2474f74cdd3
zlmonroe <zachary.lynn.monroe@gmail.com>
2017-03-11 02:47:56 -0700

    Update Station.java

--- Station.java
+++ Station.java
@@ -1,31 +1,117 @@
-public class Station {
-	private int latitude;
-	private int longitude;
-	private int temp_c;
-	private int elevation_m;
+import java.net.*;
+import java.io.*;
 
-	public int getLatitude() {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.ArrayList;
+
+public class Station {
+	private double latitude;
+	private double longitude;
+	private double temp_c;
+	private double elevation_m;
+	
+	public static void main(String args[]) {
+		for (Station s : parseCurrent()) {
+			System.out.println(s);
+		}
+	}
+	
+	public static Station[] parseCurrent() {
+		String fullXML = getMetars(25,-100,65,-100);
+		
+	    Pattern metar = regexForTag("METAR");
+	    Matcher fullXMLMatcher = metar.matcher(fullXML);
+	    
+	    ArrayList<Station> stations = new ArrayList<>();
+	    
+	    while (fullXMLMatcher.find()) {
+	    	Station currentStation = new Station();
+	    	
+	    	String stationXML = fullXMLMatcher.group(1);
+	    	String[] dataTags = new String[]{"latitude", "longitude", "temp_c", "elevation_m"};
+	    	Pattern[] dataPatterns = new Pattern[dataTags.length];
+	    	for (int i = 0; i < dataTags.length; i++) {
+	    		dataPatterns[i] = regexForTag(dataTags[i]);
+	    	}
+	       
+	    	for (int i = 0; i < dataTags.length; i++) {
+	    		Matcher dataMatcher = dataPatterns[i].matcher(stationXML);
+	    		while(dataMatcher.find()) {
+	    			switch(i) {
+	    			//doesn't verify int
+	    			case 0: currentStation.setLatitude(Double.parseDouble(dataMatcher.group(1)));
+	    			case 1: currentStation.setLongitude(Double.parseDouble(dataMatcher.group(1)));
+	    			case 2: currentStation.setTemp_c(Double.parseDouble(dataMatcher.group(1)));
+	    			case 3: currentStation.setElevation_m(Double.parseDouble(dataMatcher.group(1)));
+	    			}
+	    		}
+	    	}
+	    	stations.add(currentStation);
+	       
+	    }
+	    
+	   return stations.toArray(new Station[stations.size()]);
+	}
+	
+	public static Pattern regexForTag(String tag) {
+		return Pattern.compile("<"+tag+">(.*?)</"+tag+">", Pattern.DOTALL);
+	}
+	
+	public static String getMetars(int minLat, int minLong, int maxLat, int maxLong) {
+		String url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&minLat=" + minLat + "&minLon=" + minLong + "&maxLat=" + maxLat + "&maxLon=" + maxLong + "&hoursBeforeNow=3";
+		return getUrlSource(url);
+	}
+
+	public static String getUrlSource(String url) {
+		try {
+			URL yahoo = new URL(url);
+			URLConnection yc = yahoo.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
+			String inputLine;
+			StringBuilder a = new StringBuilder();
+			while ((inputLine = in.readLine()) != null)
+				a.append(inputLine);
+			in.close();
+
+			return a.toString();
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public double getLatitude() {
 		return latitude;
 	}
-	public int getLongitude() {
+	public double getLongitude() {
 		return longitude;
 	}
-	public int getTemp_c() {
+	public double getTemp_c() {
 		return temp_c;
 	}
-	public int getElevation_m() {
+	public double getElevation_m() {
 		return elevation_m;
 	}
-	public void setLatitude(int Latitude) {
+	public void setLatitude(double Latitude) {
 		latitude = Latitude;
 	}
-	public void setLongitude(int Longitude) {
+	public void setLongitude(double Longitude) {
 		longitude = Longitude;
 	}
-	public void setTemp_c(int Temp_c) {
+	public void setTemp_c(double Temp_c) {
 		temp_c = Temp_c;
 	}
-	public void setElevation_m(int Elevation_m) {
+	public void setElevation_m(double Elevation_m) {
 		elevation_m = Elevation_m;
 	}
+	
+	public String toString() {
+		return "Latitude: "+this.latitude+"\n"+
+				"Longitute: "+this.longitude+"\n"+
+				"Elevation: "+this.elevation_m+"\n"+
+				"Temp in Celcius: "+this.temp_c;
+	}
 }
+

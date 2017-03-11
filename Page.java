3bfcaeb71cd2c835c22ee96f8271cd49f7b66362
sullivan-wenger <swenger@asu.edu>
2017-03-11 12:02:24 -0700

    Getting google map through page

---
+++ Page.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+ 
+public class Page extends Application {
+
+    @Override
+    public void start(Stage stage) {
+	String page = "file:///home/sullivan/Desktop/mapPage.html"; //change this to the location of mapPage
+        stage.setTitle("HTML");
+        stage.setWidth(500);
+        stage.setHeight(500);
+        Scene scene = new Scene(new Group());
+        VBox root = new VBox();    
+        final WebView browser = new WebView();
+        final WebEngine webEngine = browser.getEngine();
+        webEngine.load(page);
+        root.getChildren().addAll(browser);
+        scene.setRoot(root);
+ 
+        stage.setScene(scene);
+        stage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+

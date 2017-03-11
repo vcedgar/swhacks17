dd77f3eeaccfa10a87ac3b353672e3eabb36ccbb
zlmonroe <zachary.lynn.monroe@gmail.com>
2017-03-11 13:31:01 -0700

    Update Station.java

--- Station.java
+++ Station.java
@@ -12,8 +12,8 @@ import java.util.Arrays;
  *
  */
 public class Station {
-	private static double personLat=33.3;
-	private static double personLon=-110.8;
+	private static double personLat;
+	private static double personLon;
 	private double latitude;
 	private double longitude;
 	private double temp_f;
@@ -21,7 +21,16 @@ public class Station {
 	private String date;
 	
 	public static void main(String args[]) {
+		//example
+		
+		//set person's location to pinal mountain near globe
+		Station.setPersonLat(33.3);
+		Station.setPersonLon(-110.8);
+	
+		//get list of forecasts	
 		Station[] current = parseCurrent();
+		
+		//print list of forecasts
 		System.out.println(Arrays.toString(current));
 	}
 	
@@ -31,30 +40,41 @@ public class Station {
 	 * @return array of stations
 	 */
 	private static Station[] parseCurrent() {
+		//get the hml file from the website from the nearest station to the person
 		String fullXML = getMetars(personLat, personLon);
 		
+		//parse the station's location data
 		Pattern location = Pattern.compile("<Forecast location=(.*?)>", Pattern.DOTALL);
 		Matcher locationMatch = location.matcher(fullXML);
 		
+		//seperate the numbers pattern
 		Pattern numbers = Pattern.compile("\"([0-9.]+).*?\"");
 		
+		//create a list of stations data
 		ArrayList<Station> stations = new ArrayList<>();
 		
+		//lists of strings from parsed numbers
 		String[] info = new String[4];
+		
+		//if a location was actually found
 		if (locationMatch.find()) {
+			//match numbers
 			Matcher numbersMatch = numbers.matcher(locationMatch.group(1));
 		
+			//fill the array of strings with the found numbers
 			int counter = 0;
 			while(numbersMatch.find()) {
 				info[counter] = numbersMatch.group(1);
 				counter++;
 			}
 
-		
+			//find all text for a period of forecast
 			Pattern period = regexForTag("period");
 			Matcher periodMatch = period.matcher(fullXML);
 			
+			//for each period
 			while(periodMatch.find()) {
+				//setup a station  object reflecting data
 				Station current = new Station();
 				current.setLatitude(Double.parseDouble(info[1]));
 				current.setLongitude(Double.parseDouble(info[2]));
@@ -73,7 +93,7 @@ public class Station {
 				stations.add(current);
 			}
 		}
-		
+		//return as array of station data
 	    return stations.toArray(new Station[stations.size()]);
 	}
 	
@@ -86,16 +106,21 @@ public class Station {
 		return Pattern.compile("<"+tag+">(.*?)</"+tag+">", Pattern.DOTALL);
 	}
 	/**
-	 * 
+	 * create request for api
 	 * @param d
 	 * @param e
 	 * @return XML string
 	 */
 	private static String getMetars(double d, double e) {
+		//api website +lon and lat data
 		String url = "http://forecast.weather.gov/MapClick.php?lat="+d+"&lon="+e+"&FcstType=xml";
 		return getUrlSource(url);
 	}
-
+	/**
+	 * connect to a website and retreive info as string
+	 * @param url
+	 * @return
+	 */
 	private static String getUrlSource(String url) {
 		try {
 			URL yahoo = new URL(url);
@@ -114,6 +139,8 @@ public class Station {
 		}
 	}
 	
+	//Begin getters
+	
 	public double getLatitude() {
 		return latitude;
 	}
@@ -129,6 +156,15 @@ public class Station {
 	public String getDate() {
 		return date;
 	}
+	public static double getPersonLat() {
+		return personLat;
+	}
+	public static double getPersonLon() {
+		return personLon;
+	}
+	
+	//Begin setters
+	
 	public void setLatitude(double Latitude) {
 		latitude = Latitude;
 	}
@@ -144,7 +180,14 @@ public class Station {
 	public void setDate(String date) {
 		this.date = date;
 	}
+	public static void setPersonLat(double lat) {
+		personLat = lat;
+	}
+	public static void setPersonLon(double lon) {
+		personLon = lon;
+	}
 	
+	//to String method for easy printing
 	public String toString() {
 		return "{Latitude: "+this.latitude+", "+
 				"Longitute: "+this.longitude+", "+

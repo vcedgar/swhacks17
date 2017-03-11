8406519e2b297047ee6ba05217f6fa741df35047
sullivan-wenger <swenger@asu.edu>
2017-03-11 02:07:22 -0700

    Start of station class

---
+++ Station.java
@@ -0,0 +1,31 @@
+public class Station {
+	private int latitude;
+	private int longitude;
+	private int temp_c;
+	private int elevation_m;
+
+	public int getLatitude() {
+		return latitude;
+	}
+	public int getLongitude() {
+		return longitude;
+	}
+	public int getTemp_c() {
+		return temp_c;
+	}
+	public int getElevation_m() {
+		return elevation_m;
+	}
+	public void setLatitude(int Latitude) {
+		latitude = Latitude;
+	}
+	public void setLongitude(int Longitude) {
+		longitude = Longitude;
+	}
+	public void setTemp_c(int Temp_c) {
+		temp_c = Temp_c;
+	}
+	public void setElevation_m(int Elevation_m) {
+		elevation_m = Elevation_m;
+	}
+}

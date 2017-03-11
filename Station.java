f0607dd2ff61e51eac68463e30c4aaf34fd6c6db
zlmonroe <zachary.lynn.monroe@gmail.com>
2017-03-11 13:04:52 -0700

    Redone for new API

--- Station.java
+++ Station.java
@@ -5,66 +5,98 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import java.util.ArrayList;
-
+import java.util.Arrays;
+/**
+ * Station class containing basic readings from stations
+ * @author zachary, Abdul
+ *
+ */
 public class Station {
+	private static double personLat=33.3;
+	private static double personLon=-110.8;
 	private double latitude;
 	private double longitude;
-	private double temp_c;
+	private double temp_f;
 	private double elevation_m;
+	private String date;
 	
 	public static void main(String args[]) {
-		for (Station s : parseCurrent()) {
-			System.out.println(s);
-		}
+		Station[] current = parseCurrent();
+		System.out.println(Arrays.toString(current));
 	}
 	
-	public static Station[] parseCurrent() {
-		String fullXML = getMetars(25,-100,65,-100);
+	
+	/**
+	 * get an updated array of stations from ADDS
+	 * @return array of stations
+	 */
+	private static Station[] parseCurrent() {
+		String fullXML = getMetars(personLat, personLon);
 		
-	    Pattern metar = regexForTag("METAR");
-	    Matcher fullXMLMatcher = metar.matcher(fullXML);
-	    
-	    ArrayList<Station> stations = new ArrayList<>();
-	    
-	    while (fullXMLMatcher.find()) {
-	    	Station currentStation = new Station();
-	    	
-	    	String stationXML = fullXMLMatcher.group(1);
-	    	String[] dataTags = new String[]{"latitude", "longitude", "temp_c", "elevation_m"};
-	    	Pattern[] dataPatterns = new Pattern[dataTags.length];
-	    	for (int i = 0; i < dataTags.length; i++) {
-	    		dataPatterns[i] = regexForTag(dataTags[i]);
-	    	}
-	       
-	    	for (int i = 0; i < dataTags.length; i++) {
-	    		Matcher dataMatcher = dataPatterns[i].matcher(stationXML);
-	    		while(dataMatcher.find()) {
-	    			switch(i) {
-	    			//doesn't verify int
-	    			case 0: currentStation.setLatitude(Double.parseDouble(dataMatcher.group(1)));
-	    			case 1: currentStation.setLongitude(Double.parseDouble(dataMatcher.group(1)));
-	    			case 2: currentStation.setTemp_c(Double.parseDouble(dataMatcher.group(1)));
-	    			case 3: currentStation.setElevation_m(Double.parseDouble(dataMatcher.group(1)));
-	    			}
-	    		}
-	    	}
-	    	stations.add(currentStation);
-	       
-	    }
-	    
-	   return stations.toArray(new Station[stations.size()]);
+		Pattern location = Pattern.compile("<Forecast location=(.*?)>", Pattern.DOTALL);
+		Matcher locationMatch = location.matcher(fullXML);
+		
+		Pattern numbers = Pattern.compile("\"([0-9.]+).*?\"");
+		
+		ArrayList<Station> stations = new ArrayList<>();
+		
+		String[] info = new String[4];
+		if (locationMatch.find()) {
+			Matcher numbersMatch = numbers.matcher(locationMatch.group(1));
+		
+			int counter = 0;
+			while(numbersMatch.find()) {
+				info[counter] = numbersMatch.group(1);
+				counter++;
+			}
+
+		
+			Pattern period = regexForTag("period");
+			Matcher periodMatch = period.matcher(fullXML);
+			
+			while(periodMatch.find()) {
+				Station current = new Station();
+				current.setLatitude(Double.parseDouble(info[1]));
+				current.setLongitude(Double.parseDouble(info[2]));
+				current.setElevation_m(Double.parseDouble(info[3]));
+				
+				Pattern temp = Pattern.compile("<temp .*?>([0-9.]+?)</temp>",Pattern.DOTALL);
+				Matcher tempMatcher = temp.matcher(periodMatch.group(1));
+				tempMatcher.find();
+				current.setTemp_f(Double.parseDouble(tempMatcher.group(1)));
+				
+				Pattern dateP = regexForTag("valid");
+				Matcher dateMatcher = dateP.matcher(periodMatch.group(1));
+				dateMatcher.find();
+				current.setDate(dateMatcher.group(1));
+				
+				stations.add(current);
+			}
+		}
+		
+	    return stations.toArray(new Station[stations.size()]);
 	}
 	
-	public static Pattern regexForTag(String tag) {
+	/**
+	 * returns a regex pattern for data between tags
+	 * @param tag
+	 * @return pattern for inside tags
+	 */
+	private static Pattern regexForTag(String tag) {
 		return Pattern.compile("<"+tag+">(.*?)</"+tag+">", Pattern.DOTALL);
 	}
-	
-	public static String getMetars(int minLat, int minLong, int maxLat, int maxLong) {
-		String url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&minLat=" + minLat + "&minLon=" + minLong + "&maxLat=" + maxLat + "&maxLon=" + maxLong + "&hoursBeforeNow=3";
+	/**
+	 * 
+	 * @param d
+	 * @param e
+	 * @return XML string
+	 */
+	private static String getMetars(double d, double e) {
+		String url = "http://forecast.weather.gov/MapClick.php?lat="+d+"&lon="+e+"&FcstType=xml";
 		return getUrlSource(url);
 	}
 
-	public static String getUrlSource(String url) {
+	private static String getUrlSource(String url) {
 		try {
 			URL yahoo = new URL(url);
 			URLConnection yc = yahoo.openConnection();
@@ -88,11 +120,14 @@ public class Station {
 	public double getLongitude() {
 		return longitude;
 	}
-	public double getTemp_c() {
-		return temp_c;
+	public double getTemp_f() {
+		return temp_f;
 	}
 	public double getElevation_m() {
 		return elevation_m;
+	}
+	public String getDate() {
+		return date;
 	}
 	public void setLatitude(double Latitude) {
 		latitude = Latitude;
@@ -100,37 +135,21 @@ public class Station {
 	public void setLongitude(double Longitude) {
 		longitude = Longitude;
 	}
-	public void setTemp_c(double Temp_c) {
-		temp_c = Temp_c;
+	public void setTemp_f(double Temp_f) {
+		temp_f = Temp_f;
 	}
 	public void setElevation_m(double Elevation_m) {
 		elevation_m = Elevation_m;
 	}
+	public void setDate(String date) {
+		this.date = date;
+	}
 	
 	public String toString() {
-		return "Latitude: "+this.latitude+"\n"+
-				"Longitute: "+this.longitude+"\n"+
-				"Elevation: "+this.elevation_m+"\n"+
-				"Temp in Celcius: "+this.temp_c;
-	}
-	private static double getDistance(double la1, double lo1, double la2, double lo2)
-	{
-		double distance = Math.acos(Math.sin(deg2rad(la1)) * Math.sin(deg2rad(la2)) + 
-				Math.cos(deg2rad(la1)) * Math.cos(deg2rad(la2)) * Math.cos(deg2rad(lo2 - lo1)));
-		return (rad2deg(distance) * 60.0 * 1.1515);
-	}
-	private static double deg2rad(double deg)
-	{
-		return (deg * Math.PI/180.0);
-	}
-	private static double rad2deg(double rad)
-	{
-		return (rad * 180.0 / Math.PI);
-	}
-	private static double getTempInFahrenheit(double temp)
-	{
-		temp = ((temp * 9 / 5.0) + 32);
-		return temp;
+		return "{Latitude: "+this.latitude+", "+
+				"Longitute: "+this.longitude+", "+
+				"Elevation: "+this.elevation_m+", "+
+				"Temp in Fahrenheit: "+this.temp_f+", "+
+				"Date: "+date+"}";
 	}
 }
-

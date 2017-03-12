d1c1c9cf1223d410376b13e5dd726eeefd920842
vcedgar <vatriciaedgar@gmail.com>
2017-03-12 02:04:55 -0700

    Update Controller.java

--- graphics/Controller.java
+++ graphics/Controller.java
@@ -1,4 +1,6 @@
 package application;
+import java.text.DecimalFormat;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,7 +11,10 @@ public class Controller {
 	private Button enterButton;
 	
 	@FXML
-	private Label outputLabel;
+	private Label outputDay;
+	
+	@FXML
+	private Label outputTemp;
 	
 	@FXML
 	private TextField ladField;
@@ -20,10 +25,30 @@ public class Controller {
 	//enter the code for display here
 	public void enterButtonListener(){
 		if(ladField.getLength() == 0 || longField.getLength() == 0){
-			outputLabel.setText("Invalid");
+			outputDay.setText("Invalid");
+			outputTemp.setText("Invalid");
+			System.out.println("Invalid");
 		}
 		else{
-			outputLabel.setText("Valid");
+			DecimalFormat form = new DecimalFormat("#.##");
+			double laditude = Double.parseDouble(ladField.getText());
+			double longitude = Double.parseDouble(longField.getText());
+			Station.setPersonLat(laditude);
+			Station.setPersonLon(longitude);
+			Forecast[] myFore = Station.getForecasts(Station.parseCurrent());
+			String messageDay = "";
+			String messageTemp = "";
+			for(int i = 0; i < myFore.length;i++){
+				messageDay = messageDay + "Day: " + myFore[i].date + "\n";
+			}
+			for(int i = 0; i < myFore.length;i++){
+				messageTemp = messageTemp + "Temperature: " + form.format(myFore[i].temp) + "\n";
+			}
+
+			outputDay.setText(messageDay);
+			System.out.println(messageDay);
+			outputTemp.setText(messageTemp);
+			System.out.println(messageTemp);
 		}
 	}
 }

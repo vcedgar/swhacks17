48241ea9e2abe678da1af5e41ae29b586df62ca3
vcedgar <vatriciaedgar@gmail.com>
2017-03-11 19:05:36 -0700

    Update Controller.java

--- graphics/Controller.java
+++ graphics/Controller.java
@@ -1,5 +1,29 @@
 package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
+	@FXML 
+	private Button enterButton;
+	
+	@FXML
+	private Label outputLabel;
+	
+	@FXML
+	private TextField ladField;
+	
+	@FXML
+	private TextField longField;
 
+	//enter the code for display here
+	public void enterButtonListener(){
+		if(ladField.getLength() == 0 || longField.getLength() == 0){
+			outputLabel.setText("Invalid");
+		}
+		else{
+			outputLabel.setText("Valid");
+		}
+	}
 }
